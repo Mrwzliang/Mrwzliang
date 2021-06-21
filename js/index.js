82e96ca30d1c1fbd9882ad282761c42a8f73e70b
godWzl <31930950+Mrwzliang@users.noreply.github.com>
2021-06-21 22:47:14 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|wzl❤hxm|welcome|');
+      S.UI.simulate('|wzl❤hxm|');
     }
 
     S.Drawing.loop(function () {
