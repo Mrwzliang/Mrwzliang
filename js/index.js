71491d4dec7badebbc57db368c84ac85d783b54d
godWzl <31930950+Mrwzliang@users.noreply.github.com>
2021-09-12 19:00:44 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|wzl❤hcm||wzl❤hcm|wzl❤hcm|wzl❤hcm|wzl❤hcm|wzl❤hcm|');
+      S.UI.simulate('|AN IC VERIFIER|');
     }
 
     S.Drawing.loop(function () {
